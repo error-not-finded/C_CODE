10b9dee6a5c7a9aab243bf3c10544dc8064cf227
error-not-finded <1156599046@qq.com>
2021-04-16 17:53:00 +0800

    猜凶手

---
+++ 猜凶手/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd'; killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("%c", killer);
+//		}
+//	}
+//	return 0;
+//}
+int main()
+{
+	int arr[5] = { 0 };
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		int j = 0;
+		for (j = 'a'; j <= 'e'; j++)
+		{
+
+		}
+	}
+	return 0;
+}
